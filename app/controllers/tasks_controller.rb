4dd7b599bcb26b675af30f7ef0eaae58f16203bb
pierre groleau <groleau.pierre@gmail.com>
2020-11-17 14:56:57 +0100

    ajout du create

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+
+    redirect_to tasks_path
+  end
+
+  def index
+  @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
